710f6cba068de9c92f3263055912f6ec5995b6d5
hellobye65536 <boshwang1234@gmail.com>
2025-12-04 00:28:57 -0500

    p4

---
+++ src/P4a.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Exception (assert)
+import Data.Array.IArray qualified as A
+import Data.Array.Unboxed (UArray)
+import Data.List (group)
+import Data.Maybe (fromMaybe)
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+
+parse :: Char -> Int
+parse '@' = 1
+parse _ = 0
+
+makeGrid :: [[Int]] -> UArray (Int, Int) Int
+makeGrid xs =
+  assert (length (group $ map length xs) == 1) $
+    A.listArray ((1, 1), (length xs, length $ head xs)) $
+      concat xs
+
+solve :: UArray (Int, Int) Int -> UArray (Int, Int) Bool
+solve grid = A.genArray (A.bounds grid) $ \(r, c) ->
+  (grid A.! (r, c) == 1)
+    && ( sum
+           ( map (fromMaybe 0 . (grid A.!?)) $
+               [(r + dr, c + dc) | dr <- [-1 .. 1], dc <- [-1 .. 1], dr /= 0 || dc /= 0]
+           )
+           < 4
+       )
+
+main :: IO ()
+main =
+  T.interact $
+    (<> "\n")
+      . T.show
+      . length
+      . filter id
+      . A.elems
+      . solve
+      . makeGrid
+      . map (map parse . T.unpack)
+      . T.lines
+      . T.strip
---
+++ src/P4b.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Exception (assert)
+import Data.Array.IArray qualified as A
+import Data.Array.Unboxed (UArray)
+import Data.Bifunctor (Bifunctor (second))
+import Data.List (group)
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+
+boolInt :: Bool -> Int
+boolInt False = 0
+boolInt True = 1
+
+makeGrid :: [[Bool]] -> UArray (Int, Int) Bool
+makeGrid xs =
+  assert (length (group $ map length xs) == 1) $
+    A.listArray ((1, 1), (length xs, length $ head xs)) $
+      concat xs
+
+step :: UArray (Int, Int) Bool -> UArray (Int, Int) Bool
+step grid = A.genArray (A.bounds grid) $ \(r, c) ->
+  (grid A.! (r, c))
+    && ( sum
+           ( map (maybe 0 boolInt . (grid A.!?)) $
+               [(r + dr, c + dc) | dr <- [-1 .. 1], dc <- [-1 .. 1], dr /= 0 || dc /= 0]
+           )
+           < 4
+       )
+
+solve :: UArray (Int, Int) Bool -> Int
+solve grid = stepCnt + if stepCnt == 0 then 0 else solve grid'
+ where
+  stepped = step grid
+  stepCnt = length $ filter id $ A.elems stepped
+  grid' = grid A.// map (second $ const False) (filter snd $ A.assocs stepped)
+
+main :: IO ()
+main =
+  T.interact $
+    (<> "\n")
+      . T.show
+      . solve
+      . makeGrid
+      . map (map (== '@') . T.unpack)
+      . T.lines
+      . T.strip
